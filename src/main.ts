dbd6ea9845eeb70a4840cde0827945b2d50d56c1
dev Kim <windofwind@users.noreply.github.com>
2024-08-21 02:08:21 +0000

    add websocket

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { WebSocketAdaptor } from '@nestia/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  await WebSocketAdaptor.upgrade(app);
   await app.listen(3000);
 }
 bootstrap();
